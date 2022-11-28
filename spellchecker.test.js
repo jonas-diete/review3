cbac08cc02610a6244db4fcfc127be2e38f45af9
Jonas Diete <jonas.diete@gmail.com>
2022-11-28 12:29:42 +0000

    two words in any combination returned correctly

--- spellchecker.test.js
+++ spellchecker.test.js
@@ -5,11 +5,19 @@ describe('Spellchecker', () => {
     expect(spellcheck('hello')).toBe('hello');
   })
 
-  it('returns a correctly spelled word', () => {
+  it('returns two correctly spelled words', () => {
     expect(spellcheck('good morning')).toBe('good morning');
   })
 
   it('returns a misspelt word with ~', () => {
     expect(spellcheck('helbo')).toBe('~helbo~');
+  })
+
+  it('returns two misspelt words ~', () => {
+    expect(spellcheck('helbo worbd')).toBe('~helbo~ ~worbd~');
+  })
+
+  it('returns correct and misspelt word ~', () => {
+    expect(spellcheck('hello worbd')).toBe('hello ~worbd~');
   })
 })
